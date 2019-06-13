f1ae9569b3e95dd85808050b0cdb58150bb05e7c
Dunyoo <hwj19980808@gmail.com>
2019-06-13 20:32:25 +0800

    Add files via upload

---
+++ reverse_string/src/reversestring/string.java
@@ -0,0 +1,20 @@
+package reversestring;
+
+import java.lang.*; 
+import java.io.*; 
+import java.util.*; 
+public class string {
+	public static void main(String[] args) 
+	    { 
+	        String input = "Can you reverse?"; 
+	  
+	        StringBuilder input1 = new StringBuilder(); 
+
+	        input1.append(input); 
+	 
+	        input1 = input1.reverse(); 
+	  
+	        System.out.println(input1); 
+	    } 
+
+}
